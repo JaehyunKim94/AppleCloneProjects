1a75ce4cab8ee9969b55145d459a1917cc500a04
JaehyunKim94 <didwjd169@gmail.com>
2021-02-22 21:19:50 +0900

    Update js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (() => {
+
+    let yOffset = 0;    // window.pageYOffset 대신 씀 (스크롤 위치)
+    let prevScrollHeight = 0;   // 현재 스크롤 위치보다 이전에 위치한 스크롤 섹션들의 높이 합
+    let currentScene = 0;   // 현재 활성화된 스크롤 섹션(씬)
+
     const sceneInfo = [
         {
             // 0
@@ -43,9 +48,30 @@
         for (let i = 0; i < sceneInfo.length; i++) {
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
+        };
+    };
+
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        };
+        
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            // 스크롤이 내려갈 때
+            currentScene++;
         }
-        console.log(sceneInfo);
-    }
+        if(yOffset < prevScrollHeight) {
+            // 스크롤이 올라갈 때
+            if (currentScene == 0) return;
+            currentScene--;
+        };
+        console.log(currentScene)
+    };
 
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
 })();
